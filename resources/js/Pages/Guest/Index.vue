e40ca1f6df554d1d4d5751d6014c6e0eac56a52b
Nikita V <swebs.sh@gmail.com>
2024-12-11 20:41:13 +0500

    Изменил ссылку для QR кода

--- resources/js/Pages/Guest/Index.vue
+++ resources/js/Pages/Guest/Index.vue
@@ -3,16 +3,17 @@ import QrcodeVue from 'qrcode.vue'
 </script>
 
 <template>
-    <div class="h-screen w-screen overflow-hidden bg-gradient-to-r from-cyan-500 to-blue-500 flex flex-col items-center justify-center p-8">
+    <div
+        class="h-screen w-screen overflow-hidden bg-gradient-to-r from-cyan-500 to-blue-500 flex flex-col items-center justify-center p-8">
         <div>
             <div class="flex gap-2 items-center bg-white px-8 py-2 rounded-full">
                 <img src="/assets/logo/Logo-SP.svg" alt="" class="w-12">
-                <span class="leading-4 font-bold text-blue-900">STUDENTTIK <br /> EMHANA <br /><small class="text-[10px]">TTS</small></span>
+                <span class="leading-4 font-bold text-blue-900">STUDENTTIK <br/> EMHANA <br/><small class="text-[10px]">TTS</small></span>
 
             </div>
         </div>
         <div class="bg-white w-full sm:w-2/4 flex flex-col items-center justify-center rounded-lg box-border p-8 mt-4">
-            <QrcodeVue value="/" :size="240"/>
+            <QrcodeVue :value="route('dashboard')" :size="240"/>
         </div>
     </div>
 </template>
